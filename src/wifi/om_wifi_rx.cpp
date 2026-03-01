2232241c1d8b468d123aca754e7595696b9d605e
__int__ <2726087873@qq.com>
2026-03-02 01:49:15 +0800

    RX

--- src/wifi/om_wifi_rx.cpp
+++ src/wifi/om_wifi_rx.cpp
@@ -1,29 +1,35 @@
 #include "om_wifi_rx.h"
 #include <WiFi.h>
+#include "om_status_led.h"
 
 extern "C" {
   #include "esp_wifi.h"
 }
 #include <esp_now.h>
 
-// extern DataPacket data;
+typedef struct {
+  int value;
+} DataPacket;
+
+DataPacket data;
+
 
 static const uint8_t OM_WIFI_CH = 1;
 static volatile uint32_t g_rxCount = 0;
 
-static void onReceive(const uint8_t *mac, const uint8_t *incomingData, int len) {
-  if (len != (int)sizeof(data)) {
-    Serial.print("[RX] bad len=");
-    Serial.println(len);
-    return;
-  }
+void onReceive(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  Serial.printf("[RX] from %02X:%02X:%02X:%02X:%02X:%02X len=%d\n",
+    mac[0],mac[1],mac[2],mac[3],mac[4],mac[5], len);
+
+  if (len != sizeof(data)) return;
 
   memcpy(&data, incomingData, sizeof(data));
-  g_rxCount++;
 
-  Serial.print("[RX] ");
-  Serial.print(g_rxCount);
-  Serial.print(" value=");
+  om_status_pulse_rx();
+  om_status_mark_link_alive();
+  om_status_set_link_state(OM_LINK_CONNECTED);
+
+  Serial.print("Received: ");
   Serial.println(data.value);
 }
 
@@ -32,8 +38,10 @@ void om_wifi_init() {
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect(true);
-  esp_wifi_set_channel(OM_WIFI_CH, WIFI_SECOND_CHAN_NONE);
-
+  WiFi.setSleep(false);
+  esp_wifi_set_ps(WIFI_PS_NONE);
+  esp_wifi_start();
+  esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
   if (esp_now_init() != ESP_OK) {
     Serial.println("[RX] ESP-NOW init failed");
     return;
@@ -41,6 +49,11 @@ void om_wifi_init() {
 
   esp_now_register_recv_cb(onReceive);
   Serial.println("[RX] ESP-NOW ready");
+
+  om_status_init(40);
+  om_status_set_link_state(OM_LINK_READY);
+  om_status_set_lost_timeout(600); // 你遥控频率高就改小，比如 300ms
+
 }
 
 void om_wifi_service() {
--- src/ws2812_screen/ws2812_screen.h
+++ src/ws2812_screen/ws2812_screen.h
@@ -1,4 +1,4 @@
-#pragma
+#pragma once
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
